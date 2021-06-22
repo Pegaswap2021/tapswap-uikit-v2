89f1b197572f571dcc223ce2e09c07e51c181e02
mark <mark@gmail.com>
2021-06-22 14:07:23 +0800

    feat: added ontowallet

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -5,6 +5,7 @@ import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
 import BinanceChain from "./icons/BinanceChain";
 import SafePalWallet from "./icons/SafePalWallet";
+import OntoWallet from "./icons/OntoWallet";
 import { Config, ConnectorNames } from "./types";
 
 const connectors: Config[] = [
@@ -41,6 +42,11 @@ const connectors: Config[] = [
   {
     title: "SafePal Wallet",
     icon: SafePalWallet,
+    connectorId: ConnectorNames.Injected,
+  },
+  {
+    title: "Onto Wallet",
+    icon: OntoWallet,
     connectorId: ConnectorNames.Injected,
   },
 ];
